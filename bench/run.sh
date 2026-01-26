1f0e4e8eea6c44ba4d3426be86e7ddc4668455fe
peterxcli <peterxcli@gmail.com>
2026-01-27 02:41:13 +0800

    add page cache purge for mac

--- bench/run.sh
+++ bench/run.sh
@@ -20,7 +20,12 @@ esac
 
 cat queries.sql | while read -r query; do
     sync
-    echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null
+    # Clear OS cache based on platform
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        sudo purge
+    else
+        echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null
+    fi
 
     echo "$query";
     cli_params=()
